7794e12e9ab5d0d479b6634ac3fee8f622997aea
lumo <email>
2020-08-20 14:45:46 +0200

    Test initiale Konfiguration abgeschlossen

--- apps/territory-offline-e2e/src/integration/initial-configuration.ts
+++ apps/territory-offline-e2e/src/integration/initial-configuration.ts
@@ -8,4 +8,36 @@ describe('InitialeKonfiguration', () =>
       .url().should('not.contain', 'territories')
   }
   )
+  it('Eingabe Name-Versammlungsgebiet', () =>
+  {
+    cy.get('.password-wrapper input').first()
+      .clear()
+      .type('Hallo')
+      .should('have.value', 'Hallo')
+  }
+  )
+  it('Eingabe/Auswahl Sprache', () =>
+  {
+    const languageInput = '.password-wrapper input[name="language"]'
+    cy.get(languageInput)
+      .type('de')
+    cy.get('app-language-search app-list-item').first()
+      .click()
+    cy.get(languageInput)
+      .should('have.value', 'Deutsch')
+  }
+  )
+  it('Button Weiter', () =>
+  {
+    cy.get('.main-button').first() //Klasse gibt immer eine Liste zurück, deswegen Auswahl mit first(), auch wenn es nur einen Button gibt
+      .click()
+  }
+  )
+  it('Button Landkarte ausrichten anklicken', () =>
+  {
+    cy.get('div.button')
+      .click()
+    cy.url().should('contain', 'dashboard')
+  }
+  )
 })
