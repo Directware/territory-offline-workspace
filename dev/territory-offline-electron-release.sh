e3e1f9583cbb086400c9476df3b82d6b65d090a9
mateusz <mklimentowicz@googlemail.com>
2020-12-08 20:36:46 +0100

    new release

--- dev/territory-offline-electron-release.sh
+++ dev/territory-offline-electron-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-branch_name=$(git symbolic-ref -q HEAD)
+branch_name=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 git add .
 git commit -m "new release"
 git push origin branch_name
