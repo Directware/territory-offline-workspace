7292be420780f39127bc79790331149d07d79674
mateusz <mail@klimentowicz.com>
2021-05-06 19:53:15 +0200

    translation improvement

--- apps/territory-offline/src/app/core/services/common/platform-agnostic-actions.service.ts
+++ apps/territory-offline/src/app/core/services/common/platform-agnostic-actions.service.ts
@@ -20,15 +20,14 @@ export class PlatformAgnosticActionsService
 
   public async share(file: any, fileName: string)
   {
-    const translations = await this.translate.get(['platformActions.sync, platformActions.sharingFailed']).toPromise();
     await Plugins.FileSharer.share({
       filename: fileName,
       base64Data: btoa(file),
       contentType: "text/plain;charset=utf-8",
       android: {
-        chooserTitle: translations['platformActions.sync']
+        chooserTitle: this.translate.instant('platformActions.sync')
       }
-    }).catch(error => console.error(translations['platformActions.sharingFailed'], error.message));
+    }).catch(error => console.error(this.translate.instant('platformActions.sharingFailed'), error.message));
   }
 
   public async shareText(text: string)
