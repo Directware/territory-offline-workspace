2363349446c8d4f797da6562af1d78069a0d993c
Mateusz Klimentowicz <m.klimentowicz@one-unity.de>
2022-09-26 14:22:59 +0200

    fix: fonts in territory offline

--- apps/territory-offline/src/app/core/services/app-initialiser.service.ts
+++ apps/territory-offline/src/app/core/services/app-initialiser.service.ts
@@ -1,66 +1,72 @@
-import {Injectable} from '@angular/core';
-import {Actions, ofType} from '@ngrx/effects';
-import {Store} from '@ngrx/store';
-import {take, tap} from 'rxjs/operators';
-import {ApplicationState} from '../store/index.reducers';
-import {LoadSettings, LoadSettingsSuccess} from '../store/settings/settings.actions';
-import {DataSecurityService} from "./common/data-security.service";
-import {DatabaseService} from "./db/database.service";
-import {SettingsDatabaseService} from "./db/settings-database.service";
-import {environment} from "../../../environments/environment";
-import {logger} from "@territory-offline-workspace/shared-utils";
+import { Injectable } from "@angular/core";
+import { Actions, ofType } from "@ngrx/effects";
+import { Store } from "@ngrx/store";
+import { take, tap } from "rxjs/operators";
+import { ApplicationState } from "../store/index.reducers";
+import {
+  LoadSettings,
+  LoadSettingsSuccess,
+} from "../store/settings/settings.actions";
+import { DataSecurityService } from "./common/data-security.service";
+import { DatabaseService } from "./db/database.service";
+import { SettingsDatabaseService } from "./db/settings-database.service";
+import { environment } from "../../../environments/environment";
+import { logger } from "@territory-offline-workspace/shared-utils";
 
-@Injectable({providedIn: 'root'})
-export class AppInitializerService
-{
-  constructor(private store: Store<ApplicationState>,
-              private dataSecurityService: DataSecurityService,
-              private settingsDatabaseService: SettingsDatabaseService,
-              private databaseService: DatabaseService,
-              private actions$: Actions)
-  {
+@Injectable({ providedIn: "root" })
+export class AppInitializerService {
+  constructor(
+    private store: Store<ApplicationState>,
+    private dataSecurityService: DataSecurityService,
+    private settingsDatabaseService: SettingsDatabaseService,
+    private databaseService: DatabaseService,
+    private actions$: Actions
+  ) {}
+
+  public async load(): Promise<any> {
+    return new Promise((resolve, reject) =>
+      this.beforeAppStart().then(() => resolve(null))
+    );
   }
 
-  public async load(): Promise<any>
-  {
-    return new Promise((resolve, reject) => this.beforeAppStart().then(() => resolve()));
-  }
-
-  private async beforeAppStart(): Promise<any>
-  {
+  private async beforeAppStart(): Promise<any> {
     this.logNgrxActions();
     await this.dataSecurityService.init();
     await this.settingsDatabaseService.initAppropriateSQLite();
     await this.databaseService
       .init()
       .then(() => logger(`Database successfully opened.`))
-      .catch((e) => console.error('####### \n\n Fehler beim öffnen der Datenbank! \n\n', e));
+      .catch((e) =>
+        console.error("####### \n\n Fehler beim öffnen der Datenbank! \n\n", e)
+      );
 
     await this.loadAppConfiguration();
     return "ready";
   }
 
-  private async loadAppConfiguration()
-  {
+  private async loadAppConfiguration() {
     const promise = new Promise((resolve, reject) =>
       this.actions$
         .pipe(
           ofType(LoadSettingsSuccess),
           take(1),
-          tap(() => resolve())
-        ).subscribe()
+          tap(() => resolve(null))
+        )
+        .subscribe()
     );
     this.store.dispatch(LoadSettings());
     return promise;
   }
 
-  private logNgrxActions()
-  {
-    if (environment.consoleLogNgrxActions)
-    {
-      this.actions$.pipe(
-        tap((action) => console.log(`[NGRX - ${action.type}]: ${JSON.stringify(action)}`))
-      ).subscribe();
+  private logNgrxActions() {
+    if (environment.consoleLogNgrxActions) {
+      this.actions$
+        .pipe(
+          tap((action) =>
+            console.log(`[NGRX - ${action.type}]: ${JSON.stringify(action)}`)
+          )
+        )
+        .subscribe();
     }
   }
 }
