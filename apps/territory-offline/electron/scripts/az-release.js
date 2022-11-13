e5a7933b99c464e9475c06f788b4d4e4ff3e2d6d
mateusz <mail@klimentowicz.com>
2022-11-13 12:07:37 +0100

    fix:  azure credentials

--- apps/territory-offline/electron/scripts/az-release.js
+++ apps/territory-offline/electron/scripts/az-release.js
@@ -1,8 +1,8 @@
-const pjson = require('./../../package.json');
-const fs = require('fs');
+const pjson = require("./../../package.json");
+const fs = require("fs");
 const baseDir = `${__dirname}/../release/`;
-const azure = require('azure-storage');
-const cliProgress = require('cli-progress');
+const azure = require("azure-storage");
+const cliProgress = require("cli-progress");
 
 /* Some important initialisations */
 const releaseNameForMac = `territory-offline.dmg`;
@@ -10,17 +10,24 @@ const releaseNameForWin = `territory-offline.exe`;
 const releaseNameForLinux = `territory-offline.AppImage`;
 const currentReleaseConfigName = "current-release.json";
 const BLOB_CONTAINER = "to-installations";
-const filesToBeDeployed = [releaseNameForMac, releaseNameForWin, releaseNameForLinux, currentReleaseConfigName];
+const filesToBeDeployed = [
+  releaseNameForMac,
+  releaseNameForWin,
+  releaseNameForLinux,
+  currentReleaseConfigName,
+];
 const startTime = new Date();
-const blobService = azure.createBlobService();
+const blobService = azure.createBlobService(
+  process.env.AZURE_STORGE_ACCOUNT,
+  process.env.AZURE_STORGE_ACCESS_KEY,
+  process.env.AZURE_HOST
+);
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 createReleaseJsonFile();
 
-filesToBeDeployed.forEach(fName =>
-{
-  if (!fs.existsSync(baseDir + fName))
-  {
+filesToBeDeployed.forEach((fName) => {
+  if (!fs.existsSync(baseDir + fName)) {
     console.log("Can not find file: " + fName);
     process.exit();
   }
@@ -28,25 +35,27 @@ filesToBeDeployed.forEach(fName =>
 
 uploadFilesToAzureBlob();
 
-function createReleaseJsonFile()
-{
-  fs.writeFile(`${baseDir + currentReleaseConfigName}`, JSON.stringify({
-    version: pjson.version,
-    winFileName: releaseNameForWin,
-    macFileName: releaseNameForMac,
-    linuxFileName: releaseNameForLinux,
-    creation: new Date()
-  }), 'utf8', () =>
-  {
-  });
+function createReleaseJsonFile() {
+  fs.writeFile(
+    `${baseDir + currentReleaseConfigName}`,
+    JSON.stringify({
+      version: pjson.version,
+      winFileName: releaseNameForWin,
+      macFileName: releaseNameForMac,
+      linuxFileName: releaseNameForLinux,
+      creation: new Date(),
+    }),
+    "utf8",
+    () => {}
+  );
 }
 
-function uploadFilesToAzureBlob()
-{
-  if (filesToBeDeployed.length === 0)
-  {
+function uploadFilesToAzureBlob() {
+  if (filesToBeDeployed.length === 0) {
     console.log("");
-    console.log(`#### Release successfully completed in ${timeDiference()}! ####`);
+    console.log(
+      `#### Release successfully completed in ${timeDiference()}! ####`
+    );
     return;
   }
 
@@ -56,23 +65,26 @@ function uploadFilesToAzureBlob()
   console.log("Start upload: " + fileName);
 
   const d = {
-    blockSize: 1024 * 1024
+    blockSize: 1024 * 1024,
   };
-  const speedSummary = blobService.createBlockBlobFromLocalFile(a, fileName, c, d, function (error, result, response)
-  {
-    if (error)
-    {
-      console.error(error);
+  const speedSummary = blobService.createBlockBlobFromLocalFile(
+    a,
+    fileName,
+    c,
+    d,
+    function (error, result, response) {
+      if (error) {
+        console.error(error);
+      }
     }
-  });
+  );
 
-  speedSummary.on('progress', function() {
+  speedSummary.on("progress", function () {
     const percentComplete = speedSummary.getCompletePercent(2);
 
     bar1.start(100, percentComplete);
 
-    if (percentComplete >= 100)
-    {
+    if (percentComplete >= 100) {
       bar1.stop();
       filesToBeDeployed.shift();
       uploadFilesToAzureBlob();
@@ -80,11 +92,10 @@ function uploadFilesToAzureBlob()
   });
 }
 
-function timeDiference()
-{
+function timeDiference() {
   const timeDiff = new Date() - startTime;
   const minutes = Math.floor(timeDiff / 1000 / 60) + "";
-  const sec = timeDiff - (minutes * 60 * 1000) + "";
+  const sec = timeDiff - minutes * 60 * 1000 + "";
 
   const formattedMins = minutes.padStart(2, "0");
   const formattedSec = sec.padStart(2, "0").slice(0, 2);
