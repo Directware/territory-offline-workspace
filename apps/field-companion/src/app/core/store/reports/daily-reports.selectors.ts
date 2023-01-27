464bff5b5a882cc6257f1393dc4f307cfa203354
Mateusz Klimentowicz <m.klimentowicz@one-unity.de>
2023-01-27 19:46:05 +0100

    fix: bugs

--- apps/field-companion/src/app/core/store/reports/daily-reports.selectors.ts
+++ apps/field-companion/src/app/core/store/reports/daily-reports.selectors.ts
@@ -1,17 +1,18 @@
-import {ApplicationState} from '../index.reducers';
-import {dailyReportsAdapter} from "./daily-reports.reducer";
-import {createSelector} from "@ngrx/store";
-import {selectSettingsFeature} from "../settings/settings.selectors";
-import {emptyDailyReport, emptyMergedDailyReport} from "@territory-offline-workspace/shared-interfaces";
+import { ApplicationState } from "../index.reducers";
+import { dailyReportsAdapter } from "./daily-reports.reducer";
+import { createSelector } from "@ngrx/store";
+import { selectSettingsFeature } from "../settings/settings.selectors";
+import {
+  emptyDailyReport,
+  emptyMergedDailyReport,
+} from "@territory-offline-workspace/shared-interfaces";
+import { getServiceYearTimes } from "@territory-offline-workspace/shared-utils";
 
-export const selectDailyReportsFeature = (state: ApplicationState) => state.dailyReports;
+export const selectDailyReportsFeature = (state: ApplicationState) =>
+  state.dailyReports;
 
-export const {
-  selectIds,
-  selectEntities,
-  selectAll,
-  selectTotal,
-} = dailyReportsAdapter.getSelectors(selectDailyReportsFeature);
+export const { selectIds, selectEntities, selectAll, selectTotal } =
+  dailyReportsAdapter.getSelectors(selectDailyReportsFeature);
 
 export const selectAllDailyReports = createSelector(
   selectAll,
@@ -22,40 +23,48 @@ export const selectNextMonthsDailyReport = createSelector(
   selectAll,
   selectDailyReportsFeature,
   (dailyReports, state) => {
-
-    return dailyReports.filter((dr) =>
-      dr.creationTime.getFullYear() === state.chosenTime.year
-      && dr.creationTime.getMonth() === state.chosenTime.month + 1
-      && dr.creationTime.getDate() === 1
-    )[0] || emptyDailyReport(state.chosenTime.year, state.chosenTime.month + 1, 1);
+    return (
+      dailyReports.filter(
+        (dr) =>
+          dr.creationTime.getFullYear() === state.chosenTime.year &&
+          dr.creationTime.getMonth() === state.chosenTime.month + 1 &&
+          dr.creationTime.getDate() === 1
+      )[0] ||
+      emptyDailyReport(state.chosenTime.year, state.chosenTime.month + 1, 1)
+    );
   }
 );
 
 export const selectCurrentDailyReports = createSelector(
   selectAll,
   selectDailyReportsFeature,
-  (reports, state) =>
-  {
-    return reports.filter(dr => dr.creationTime.getMonth() === state.chosenTime.month && dr.creationTime.getFullYear() === state.chosenTime.year);
+  (reports, state) => {
+    return reports.filter(
+      (dr) =>
+        dr.creationTime.getMonth() === state.chosenTime.month &&
+        dr.creationTime.getFullYear() === state.chosenTime.year
+    );
   }
 );
 
 export const selectCurrentDailyReportTime = createSelector(
   selectDailyReportsFeature,
   selectCurrentDailyReports,
-  (dailyReportsState, dailyReports) =>
-  {
+  (dailyReportsState, dailyReports) => {
     const tmp = [];
-    dailyReports.forEach(dr =>
-    {
-      if (dr.duration > 0 || dr.deliveries > 0 || dr.returnVisits > 0 || dr.videos > 0)
-      {
-        tmp.push(dr.creationTime)
+    dailyReports.forEach((dr) => {
+      if (
+        dr.duration > 0 ||
+        dr.deliveries > 0 ||
+        dr.returnVisits > 0 ||
+        dr.videos > 0
+      ) {
+        tmp.push(dr.creationTime);
       }
     });
     return {
       ...dailyReportsState.chosenTime,
-      dataExistOnDates: tmp
+      dataExistOnDates: tmp,
     };
   }
 );
@@ -68,56 +77,71 @@ export const selectChosenCalendarCell = createSelector(
 export const selectCurrentDailyReport = createSelector(
   selectAll,
   selectDailyReportsFeature,
-  (dailyReports, state) =>
-  {
-    if (!state.chosenCalendarCell)
-    {
+  (dailyReports, state) => {
+    if (!state.chosenCalendarCell) {
       return null;
     }
 
-    return dailyReports.filter((dr) =>
-      dr.creationTime.getFullYear() === state.chosenTime.year
-      && dr.creationTime.getMonth() === state.chosenTime.month
-      && dr.creationTime.getDate() === state.chosenCalendarCell.dayIndex
-    )[0] || emptyDailyReport(state.chosenTime.year, state.chosenTime.month, state.chosenCalendarCell.dayIndex);
-  });
+    return (
+      dailyReports.filter(
+        (dr) =>
+          dr.creationTime.getFullYear() === state.chosenTime.year &&
+          dr.creationTime.getMonth() === state.chosenTime.month &&
+          dr.creationTime.getDate() === state.chosenCalendarCell.dayIndex
+      )[0] ||
+      emptyDailyReport(
+        state.chosenTime.year,
+        state.chosenTime.month,
+        state.chosenCalendarCell.dayIndex
+      )
+    );
+  }
+);
 
 export const selectCurrentLastDayInMonthDailyReport = createSelector(
   selectAll,
   selectDailyReportsFeature,
-  (dailyReports, state) =>
-  {
-    const lastDayInMonth = new Date(state.chosenTime.year, state.chosenTime.month + 1, 0);
-    const lastMonthlyDailyReport = dailyReports.filter((dr) =>
-      dr.creationTime.getFullYear() === lastDayInMonth.getFullYear()
-      && dr.creationTime.getMonth() === lastDayInMonth.getMonth()
-      && dr.creationTime.getDate() === lastDayInMonth.getDate()
+  (dailyReports, state) => {
+    const lastDayInMonth = new Date(
+      state.chosenTime.year,
+      state.chosenTime.month + 1,
+      0
+    );
+    const lastMonthlyDailyReport = dailyReports.filter(
+      (dr) =>
+        dr.creationTime.getFullYear() === lastDayInMonth.getFullYear() &&
+        dr.creationTime.getMonth() === lastDayInMonth.getMonth() &&
+        dr.creationTime.getDate() === lastDayInMonth.getDate()
     )[0];
 
-    return lastMonthlyDailyReport || emptyDailyReport(state.chosenTime.year, state.chosenTime.month + 1, 0);
+    return (
+      lastMonthlyDailyReport ||
+      emptyDailyReport(state.chosenTime.year, state.chosenTime.month + 1, 0)
+    );
   }
 );
 
 export const selectMergedCurrentDailyReports = createSelector(
   selectCurrentDailyReports,
-  (reports) =>
-  {
+  (reports) => {
     const mergedEmptyDailyReport = emptyMergedDailyReport();
-    if (!reports || reports.length === 0)
-    {
+    if (!reports || reports.length === 0) {
       return mergedEmptyDailyReport;
     }
 
-    const tmp = reports.reduce((all, item) => all = {
-      videos: all.videos + item.videos,
-      returnVisits: all.returnVisits + item.returnVisits,
-      duration: all.duration + item.duration,
-      deliveries: all.deliveries + item.deliveries,
-      studies: all.studies + item.studies,
-      creationTime: null,
-      id: null,
-      prefix: all.prefix
-    });
+    const tmp = reports.reduce(
+      (all, item) =>
+        (all = {
+          videos: all.videos + item.videos,
+          returnVisits: all.returnVisits + item.returnVisits,
+          duration: all.duration + item.duration,
+          deliveries: all.deliveries + item.deliveries,
+          studies: all.studies + item.studies,
+          creationTime: null,
+          id: null,
+          prefix: all.prefix,
+        })
+    );
 
     return {
       ...mergedEmptyDailyReport,
@@ -125,24 +149,25 @@ export const selectMergedCurrentDailyReports = createSelector(
       returnVisits: tmp.returnVisits,
       duration: tmp.duration,
       deliveries: tmp.deliveries,
-      studies: tmp.studies
-    }
-  });
+      studies: tmp.studies,
+    };
+  }
+);
 
 export const selectCurrentDailyReportStudies = createSelector(
   selectMergedCurrentDailyReports,
-  (mergedReports) => ({value: mergedReports.studies})
+  (mergedReports) => ({ value: mergedReports.studies })
 );
 
 export const selectDailyReportForStudy = createSelector(
   selectCurrentDailyReports,
   selectDailyReportsFeature,
-  (allCurrentDailyReports, state) =>
-  {
-    const firstReportInTheMonth = allCurrentDailyReports.filter(dr => dr.creationTime.getDate() === 1)[0];
+  (allCurrentDailyReports, state) => {
+    const firstReportInTheMonth = allCurrentDailyReports.filter(
+      (dr) => dr.creationTime.getDate() === 1
+    )[0];
 
-    if (!firstReportInTheMonth)
-    {
+    if (!firstReportInTheMonth) {
       return emptyDailyReport(state.chosenTime.year, state.chosenTime.month, 1);
     }
 
@@ -154,34 +179,40 @@ export const durationProgress = createSelector(
   selectSettingsFeature,
   selectDailyReportsFeature,
   selectAll,
-  (settings, drState, dailyReports) =>
-  {
-    const monthlyDailyReports = dailyReports.filter((dr) =>
-      dr.creationTime.getFullYear() === drState.chosenTime.year
-      && dr.creationTime.getMonth() === drState.chosenTime.month
+  (settings, drState, dailyReports) => {
+    const monthlyDailyReports = dailyReports.filter(
+      (dr) =>
+        dr.creationTime.getFullYear() === drState.chosenTime.year &&
+        dr.creationTime.getMonth() === drState.chosenTime.month
     );
 
-    const monthlySum = monthlyDailyReports.map(dr => dr.duration).reduce((all, item) => all = all + item, 0);
+    const monthlySum = monthlyDailyReports
+      .map((dr) => dr.duration)
+      .reduce((all, item) => (all = all + item), 0);
 
     /* Vom September des Vorjahres bis August des aktuellen Jahres */
-    const yearlyDailyReports = dailyReports.filter((dr) =>
-      (dr.creationTime.getFullYear() - 1 === drState.chosenTime.year && dr.creationTime.getMonth() > 7)
-      || (dr.creationTime.getFullYear() === drState.chosenTime.year && dr.creationTime.getMonth() < 8)
+    const result = getServiceYearTimes(new Date());
+    const yearlyDailyReports = dailyReports.filter(
+      (r) =>
+        r.creationTime.getTime() >= result.serviceYearStart &&
+        r.creationTime.getTime() <= result.serviceYearEnd
     );
 
-    const yearlySum = yearlyDailyReports.map(dr => dr.duration).reduce((all, item) => all = all + item, 0);
+    const yearlySum = yearlyDailyReports
+      .map((dr) => dr.duration)
+      .reduce((all, item) => (all = all + item), 0);
 
-    const monthlyHours = (monthlySum / 60);
-    const yearlyHours = (yearlySum / 60);
+    const monthlyHours = monthlySum / 60;
+    const yearlyHours = yearlySum / 60;
     return {
       monthly: (monthlyHours / settings.monthlyGoal) * 100,
       monthlyPhrase: monthlyHours + "h",
       monthlyHours: Math.floor(monthlyHours),
-      monthlyMinutes: (monthlySum - (Math.floor(monthlyHours) * 60)),
+      monthlyMinutes: monthlySum - Math.floor(monthlyHours) * 60,
       yearly: (yearlyHours / settings.yearlyGoal) * 100,
       yearlyPhrase: yearlyHours + "h",
       yearlyHours: Math.floor(yearlyHours),
-      yearlyMinutes: (yearlySum - (Math.floor(yearlyHours) * 60)),
+      yearlyMinutes: yearlySum - Math.floor(yearlyHours) * 60,
     };
   }
 );
