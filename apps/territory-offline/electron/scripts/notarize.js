33173d3310a889a375e6d7fc4a2f0d86e135160a
mateusz <mail@klimentowicz.com>
2023-07-20 21:13:33 +0200

    fix: notariaze

--- apps/territory-offline/electron/scripts/notarize.js
+++ apps/territory-offline/electron/scripts/notarize.js
@@ -8,12 +8,25 @@ exports.default = async function notarizing(context) {
   }
 
   const appName = context.packager.appInfo.productFilename;
+  const appPath = `${appOutDir}/${appName}.app`;
   console.log("\n\n\t Notarizing " + appName + "! Please wait...");
+  console.log("apple id -> " + process.env.APPLEID);
+  console.log("app path -> " + appPath);
 
   return await notarize({
-    appBundleId: "com.directware.territoryoffline",
-    appPath: `${appOutDir}/${appName}.app`,
+    tool: "notarytool",
+    appPath,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
+    teamId: process.env.APPLE_TEAM_ID,
+  }).catch((error) => {
+    console.error("\n\n\n\n");
+    console.error("FEHLER: Notarization failed!");
+    console.error("####################################");
+    console.error(error);
+    console.error("####################################");
+    console.error("\n\n\n\n");
+
+    throw new Error(error);
   });
 };
